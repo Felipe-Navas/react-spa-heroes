f9e4d64c12ba9611e69a6f346b11e66ab46d4413
Felipe Navas <felipenavas.itec@gmail.com>
2022-04-09 15:50:05 -0300

    Adding the new HeroCard component, the route for hero and the helper to get by id

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useParams, Navigate } from 'react-router-dom';
+import { getHeroById } from '../../selectors/getHeroById';
 
 export const HeroScreen = () => {
+  const { heroId } = useParams();
+
+  const hero = getHeroById(heroId);
+
+  if (!hero) {
+    return <Navigate to="/" />;
+  }
+
   return (
     <div>
       <h1>Hero Screen</h1>
+      {hero.superhero}
     </div>
   );
 };
