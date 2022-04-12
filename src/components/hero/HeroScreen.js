1156f33ff8c1373ba7ae59e22e4e4085bd88c0d3
Felipe Navas <felipenavas.itec@gmail.com>
2022-04-11 21:10:40 -0300

    Adding Search, some styles, HeroScreen and getHeroesByName

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,20 +1,57 @@
-import React from 'react';
-import { useParams, Navigate } from 'react-router-dom';
+import React, { useMemo } from 'react';
+import { useParams, Navigate, useNavigate } from 'react-router-dom';
 import { getHeroById } from '../../selectors/getHeroById';
 
 export const HeroScreen = () => {
   const { heroId } = useParams();
+  const navigate = useNavigate();
 
-  const hero = getHeroById(heroId);
+  const handleReturn = () => {
+    navigate(-1);
+  };
+
+  const hero = useMemo(() => getHeroById(heroId), [heroId]);
 
   if (!hero) {
     return <Navigate to="/" />;
   }
 
+  const { id, superhero, publisher, alter_ego, first_appearance, characters } =
+    hero;
+
+  const imagePath = `/assets/${id}.jpg`;
+
   return (
-    <div>
-      <h1>Hero Screen</h1>
-      {hero.superhero}
+    <div className="row mt-5">
+      <div className="col-4">
+        <img
+          src={imagePath}
+          className="img-thumbnail animate__animated animate__bounceInLeft"
+          alt={superhero}
+        />
+      </div>
+      <div className="col-8 animate__animated animate__fadeIn">
+        <h3>{superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <b>Alter ego: </b>
+            {alter_ego}
+          </li>
+          <li className="list-group-item">
+            <b>Publisher: </b>
+            {publisher}
+          </li>
+          <li className="list-group-item">
+            <b>First appearance: </b>
+            {first_appearance}
+          </li>
+        </ul>
+        <h5 className="mt-3">Characters</h5>
+        <p>{characters}</p>
+        <button className="btn btn-outline-info" onClick={handleReturn}>
+          Back
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,9 +1,67 @@
-import React from 'react';
+import { useMemo } from 'react';
+import queryString from 'query-string';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { useForm } from '../../hooks/useForm';
+import { getHeroesByName } from '../../selectors/getHeroesByName';
+import { HeroCard } from '../hero/HeroCard';
 
 export const SearchScreen = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const { q = '' } = queryString.parse(location.search);
+
+  const [formValues, handleInputChange] = useForm({ searchText: q });
+
+  const { searchText } = formValues;
+
+  const heroesFiltered = useMemo(() => getHeroesByName(q), [q]);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    navigate(`?q=${searchText}`);
+  };
+
   return (
-    <div>
-      <h1>Search Screen</h1>
-    </div>
+    <>
+      <h1>Search</h1>
+      <hr />
+      <div className="row">
+        <div className="col-5">
+          <h4>Search form</h4>
+          <hr />
+          <form onSubmit={handleSearch}>
+            <input
+              type="text"
+              placeholder="Find a hero"
+              className="form-control"
+              name="searchText"
+              autoComplete="off"
+              value={searchText}
+              onChange={handleInputChange}
+            />
+            <button type="submit" className="btn btn-outline-primary mt-1">
+              Find
+            </button>
+          </form>
+        </div>
+        <div className="col-7">
+          <h4>Resultados</h4>
+          <hr />
+
+          {q === '' ? (
+            <div className="alert alert-info">Find a Hero </div>
+          ) : (
+            heroesFiltered.length === 0 && (
+              <div className="alert alert-danger">No results: {q}</div>
+            )
+          )}
+
+          {heroesFiltered.map((hero) => (
+            <HeroCard key={hero.id} {...hero} />
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
